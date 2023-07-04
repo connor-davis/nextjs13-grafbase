e947c926246c739970a111e532dbbf1cb46f69ba
Connor Davis <cnnrproton@gmail.com>
2023-07-04 03:39:24 +0200

    Add more authentication functionality

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,6 @@
-import { auth } from "@clerk/nextjs";
-
 const Home = () => {
-  const { userId } = auth();
-
   return (
     <section>
-      <h1>
-        User Id: <span className="font-semibold">{userId}</span>
-      </h1>
       <h1>Categories</h1>
       <h1>Posts</h1>
       <h1>Pagination</h1>
